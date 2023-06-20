1a303f036e1550eefcfa5b4beebc33f70b8e6fad
AnndrezRivera <118026249+AnndrezRivera@users.noreply.github.com>
2023-06-20 14:32:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ df = pd.concat([df1, df2])
 
 us_states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 states = df[df['state'].isin(us_states)]['state'].unique().tolist()
+categories = input()
 
 
 @app.get("/")
