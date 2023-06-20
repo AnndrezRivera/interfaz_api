fa3120af7e9f0f0712b548081cc318cc5c185ece
AnndrezRivera <118026249+AnndrezRivera@users.noreply.github.com>
2023-06-20 14:51:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ states = df[df['state'].isin(us_states)]['state'].unique().tolist()
 
 categoria = df['categories'].str.split(', ').explode().unique().tolist()
 category = ['italian', 'burgers', 'chinese', 'hamburgers' 'hot dog', 'steakhouse', 'lunch', 'motel', 'patisserie', 'pizza', 'deli', 'diner', 'dinner', 'icecream', 'ice cream', 'hotel', 'hotels', 'seafood','cookie', 'crab house', 'cupcake', 'chocolate', 'churreria', 'cocktail', 'cocktails', 'coffee', 'coffees' 'tea', 'restaurant', 'restaurats', 'chesse', 'charcuterie', 'cafe', 'cafes', 'BBQ', 'bagle', 'bakery' 'bakerys', 'bar', 'bars', 'bar & grill', 'barbacue', 'beer' 'bistro', 'pasteleria', 'pastelerias', 'breakfast', 'brunch', 'buffet', 'burrito', 'cafeteria', 'cafeterias', 'cake', 'cakes', 'food', 'wine', 'wineries']
-categories = categoria.isin(category)]['categories'].unique().tolist()
+categories = categoria.isin(category).unique().tolist()
 
 @app.get("/")
 def index(request: Request):
