eaa7e4527d4037a8c5878458702c1e068162df96
AnndrezRivera <118026249+AnndrezRivera@users.noreply.github.com>
2023-06-20 16:37:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+df1 = pd.read_parquet('dataset/archivo1.parquet')
+df2 = pd.read_parquet('dataset/archivo2.parquet')
+df = pd.concat([df1, df2])
+
 us_states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 
 states = df[df['state'].isin(us_states)]['state'].unique().tolist()
@@ -19,7 +23,7 @@ def index(request: Request):
 
 @app.post("/")
 def index(request: Request, state: str = Form(...), categories: str = Form(...)):
-    response = requests.get(f'https://api-recomendaciones.onrender.com/?state={state}&categories={categories}')
+    response = requests.get(f'https://api-recomendaciones.onrender.com/?state={state}&categoria={categories}')
     data = response.json()
     return templates.TemplateResponse("results.html", {"request": request, "results": data})
 
@@ -29,6 +33,6 @@ def sentimiento_index(request: Request):
 
 @app.post("/sentimiento_cercano")
 def sentimiento_index(request: Request, state: str = Form(...), categories: str = Form(...)):
-    response = requests.get(f'https://api-recomendaciones.onrender.com/sentimiento_cercano?state={state}&categories={categories}')
+    response = requests.get(f'https://api-recomendaciones.onrender.com/sentimiento_cercano?state={state}&categoria={categories}')
     data = response.json()
     return templates.TemplateResponse("sentimiento_results.html", {"request": request, "results": data})
