39ce0ad2cd5abdf1ed98f90c6eb57c5f120e7368
AnndrezRivera <118026249+AnndrezRivera@users.noreply.github.com>
2023-06-19 23:15:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.templating import Jinja2Templates
 import requests
+import pandas as pd
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+df1 = pd.read_parquet('dataset/archivo1.parquet')
+df2 = pd.read_parquet('dataset/archivo2.parquet')
+df = pd.concat([df1, df2])
 
 states = df['state'].unique().tolist()
 categories = df['categories'].str.split(', ').explode().unique().tolist()
@@ -27,3 +32,4 @@ def sentimiento_index(request: Request, state: str = Form(...), categoria: str =
     response = requests.get(f'https://api-recomendaciones.onrender.com/sentimiento_cercano?state={state}&categoria={categoria}')
     data = response.json()
     return templates.TemplateResponse("sentimiento_results.html", {"request": request, "results": data})
+
