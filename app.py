0a2b60db5335487a91cf770ecd55b946ce302ea2
AnndrezRivera <118026249+AnndrezRivera@users.noreply.github.com>
2023-06-19 23:09:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import requests
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+states = df['state'].unique().tolist()
+categories = df['categories'].str.split(', ').explode().unique().tolist()
+
 @app.get("/")
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "states": states, "categories": categories})
 
 @app.post("/")
 def index(request: Request, state: str = Form(...), categoria: str = Form(...)):
@@ -15,14 +18,12 @@ def index(request: Request, state: str = Form(...), categoria: str = Form(...)):
     data = response.json()
     return templates.TemplateResponse("results.html", {"request": request, "results": data})
 
-
 @app.get("/sentimiento_cercano")
 def sentimiento_index(request: Request):
-    return templates.TemplateResponse("sentimiento_index.html", {"request": request})
+    return templates.TemplateResponse("sentimiento_index.html", {"request": request, "states": states, "categories": categories})
 
 @app.post("/sentimiento_cercano")
 def sentimiento_index(request: Request, state: str = Form(...), categoria: str = Form(...)):
     response = requests.get(f'https://api-recomendaciones.onrender.com/sentimiento_cercano?state={state}&categoria={categoria}')
     data = response.json()
     return templates.TemplateResponse("sentimiento_results.html", {"request": request, "results": data})
-
